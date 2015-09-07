901c7dd4c028dfbe3210987ea7089ac8a0620f51
freemind13 <mishclash@gmail.com>
2015-09-07 11:00:31 +0300

    finsih the BackEnd

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,26 +1,35 @@
 class StoriesController < ApplicationController
-before_action :find_story, only: [:show, :edit, :update, :destroy]
 
-def index
-@stories = Story.all.order("created_at DESC")
-end
-def show
 
+	before_action :find_story, only: [:show, :edit, :update, :destroy]
+
+	def index
+		if params[:category].blank?
+			@stories = Story.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@stories = Story.where(category_id: @category_id).order("created_at DESC")
+		end
 	end
+
+	def show
+	end
+
 	def new
-		@story = Story.new 
+		@story = Story.new
 	end
 
 	def create
 		@story = Story.new(stories_params)
+
 		if @story.save
 			redirect_to @story
 		else
 			render "New"
 		end
 	end
-	def edit
 
+	def edit
 	end
 
 	def update
@@ -28,23 +37,21 @@ def show
 			redirect_to @story
 		else
 			render "Edit"
+		end
 	end
-end
 
 	def destroy
 		@story.destroy
 		redirect_to root_path
-
 	end
 
 	private
 
-		def stories_params
-			params.require(:story).permit(:title, :description, :company, :url)
-		end
+	def stories_params
+		params.require(:story).permit(:title, :description, :company, :url, :category_id)
+	end
 
-		def find_story
-			@story = Story.find(params[:id])
-		end
+	def find_story
+		@story = Story.find(params[:id])
+	end
 end
-
